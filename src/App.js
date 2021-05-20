6fd6db42dd10dee0330450ff37471eda308029f1
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-05-20 13:31:59 -0400

    added player functionality and time stamps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 // Import Styles 
 import "./styles/app.scss";
 // Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
-
+// Import Util 
+import data from "./util";
 
 function App() {
+  // State 
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
     </div>
   );
 }
